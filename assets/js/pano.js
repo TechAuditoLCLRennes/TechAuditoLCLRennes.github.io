810c42f473033e157ec6261663d4235b207d0945
dewiweb <3625655+dewiweb@users.noreply.github.com>
2024-03-17 16:30:25 +0100

    feat: :art: test pano 2

--- assets/js/pano.js
+++ assets/js/pano.js
@@ -29,25 +29,24 @@ panorama1 = new PANOLENS.CubePanorama( [
 new PANOLENS.Viewer({container: document.querySelector(".pano1"),controlButtons: ["fullscreen"],viewIndicator: true,autoRotate: true,
 autoRotateSpeed: 0.5,}).add(panorama1);
 
-//document.querySelector(".pano2").innerHTML =""
-//path2 =""
-//format=""
-//if(typeof panorama2 !== 'undefined'){
-//  panorama2.dispose()
-//}else{
-//panorama2 = 'undefined'}
-//path2 = "../assets/virtual_cubes/cube2/";
-//format = ".jpg";
-//panorama2 = new PANOLENS.CubePanorama( [
-//    path2 + 'left1' + format,
-//    path2 + 'right1' + format,
-//    path2 + 'top1' + format,
-//    path2 + 'bottom1' + format,
-//    path2 + 'back1' + format,
-//    path2 + 'front1' + format
-//] );
-//
-//new PANOLENS.Viewer({container: document.querySelector(".pano2"),controlButtons: ["fullscreen"],viewIndicator: true,autoRotate: true,
-//autoRotateSpeed: 0.5,}).add(panorama2);
-//
-//
+document.querySelector(".pano2").innerHTML =""
+path2 =""
+format=""
+if(typeof panorama2 !== 'undefined'){
+  panorama2.dispose()
+}else{
+panorama2 = 'undefined'}
+path2 = "../assets/virtual_cubes/cube2/";
+format = ".jpg";
+panorama2 = new PANOLENS.CubePanorama( [
+    path2 + 'left1' + format,
+    path2 + 'right1' + format,
+    path2 + 'top1' + format,
+    path2 + 'bottom1' + format,
+    path2 + 'back1' + format,
+    path2 + 'front1' + format
+] );
+
+new PANOLENS.Viewer({container: document.querySelector(".pano2"),controlButtons: ["fullscreen"],viewIndicator: true,autoRotate: true,
+autoRotateSpeed: 0.5,}).add(panorama2);
+
